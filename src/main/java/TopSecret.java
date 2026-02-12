721c42dfab516ee3da82f1844376793cf6d82d4c
Poseidonspjs <103327899+Poseidonspjs@users.noreply.github.com>
2026-02-11 23:28:31 -0500

    Integration for file loading

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,5 +1,34 @@
-/**
- * Commmand Line Utility
- */
 public class TopSecret {
+
+    public static void main(String[] args) {
+        fileHandler fileHandler;
+        if (args.length == 0) {
+            System.out.println("No arguments provided. Printing file list...");
+
+        }
+        else if (args.length == 1) {
+            String fileID = args[0];
+            fileHandler = new fileHandler(fileID);
+            String fileContent = fileHandler.fileOut(); //TODO FOR CIPHER: Pass fileContent into cipher object
+                                                        // in order to get file content to decrypt
+
+            System.out.println("Processing file: " + fileID + " with Default Key.");
+
+        }
+        else if (args.length == 2) {
+            String fileID = args[0];
+            String key = args[1];
+            fileHandler = new fileHandler(fileID);
+            String fileContent = fileHandler.fileOut(); //TODO FOR CIPHER: Pass fileContent into cipher object
+                                                        // in order to get file content to decrypt
+
+            System.out.println("Processing file: " + fileID + " with Custom Key: " + key);
+
+        }
+
+        else {
+            System.out.println("Error: Too many arguments.");
+            System.out.println("Usage: java TopSecret [FileID] [Key]");
+        }
+    }
 }
