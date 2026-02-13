6c15ed6e561da7cb37d46237b3adfc4896092e5f
Daniel Louie <dlouie360@gmail.com>
2026-02-13 12:59:29 -0500

    Created tests

---
+++ src/test/java/ProgramControlTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProgramControlTest {
+    @Test
+    void parseFileIdToIndexTest() {
+        assertEquals(0, ProgramControl.parseFileIdToIndex("01"));
+        assertEquals(1, ProgramControl.parseFileIdToIndex("02"));
+        assertEquals(-1, ProgramControl.parseFileIdToIndex("A1"));
+        assertEquals(-1, ProgramControl.parseFileIdToIndex("1"));
+        assertEquals(-1, ProgramControl.parseFileIdToIndex(null));
+        assertEquals(-1, ProgramControl.parseFileIdToIndex("1"));
+        assertEquals(-1, ProgramControl.parseFileIdToIndex("AA"));
+        assertEquals(-1, ProgramControl.parseFileIdToIndex("0a"));
+    }
+
+    @Test
+    void listSortedDataFilesTest() throws Exception {
+        Files.writeString(Path.of("src/files/ProgramControlTestB.txt"), "test1");
+        Files.writeString(Path.of("src/files/ProgramControlTestA.txt"), "test2");
+        Files.writeString(Path.of("src/files/ProgramControlTestC.txt"), "test3");
+        assertEquals(List.of("ProgramControlTestA.txt", "ProgramControlTestB.txt", "ProgramControlTestC.txt"), ProgramControl.listDataFiles());
+    }
+
+
+
+}
