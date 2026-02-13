e63d1866f5e7cd71eba94e581c2fd530555b7372
Daniel Louie <dlouie360@gmail.com>
2026-02-13 11:29:13 -0500

    Aligned my use of decipher function

--- src/test/ProgramControlTest.java
+++ src/test/ProgramControlTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,6 +14,26 @@ public class ProgramControlTest {
         assertEquals(1, ProgramControl.parseFileIdToIndex("02"));
         assertEquals(-1, ProgramControl.parseFileIdToIndex("A1"));
         assertEquals(-1, ProgramControl.parseFileIdToIndex("1"));
+        assertEquals(-1, ProgramControl.parseFileIdToIndex(null));
+        assertEquals(-1, ProgramControl.parseFileIdToIndex("1"));
+        assertEquals(-1, ProgramControl.parseFileIdToIndex("AA"));
+        assertEquals(-1, ProgramControl.parseFileIdToIndex("0a"));
     }
+
+    @Test
+    void listDataFiles_returnsSortedFiles() throws Exception {
+        Path data = Paths.get("data");
+        Files.createDirectories(data);
+
+        // create fake files
+        Files.createFile(data.resolve("z.txt"));
+        Files.createFile(data.resolve("a.txt"));
+        Files.createFile(data.resolve("m.txt"));
+
+        List<String> result = ProgramControl.listDataFiles();
+
+        assertEquals(List.of("a.txt", "m.txt", "z.txt"), result);
+    }
+
 
 }
